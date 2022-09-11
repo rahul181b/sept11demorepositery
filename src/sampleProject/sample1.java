10423f866f81775d0fbb4d8536da28383a6c15dd
Rahul <Rahul@DESKTOP-RDTSFOS>
2022-09-11 15:28:14 +0530

    created by rahul

---
+++ src/sampleProject/sample1.java
@@ -0,0 +1,10 @@
+package sampleProject;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("created by rahul");
+	}
+
+}
